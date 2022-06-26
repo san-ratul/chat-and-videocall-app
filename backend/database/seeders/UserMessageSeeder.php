0dcf77beb6e2c5c61f14eb6d574f239f78e883a1
san-ratul <san.ratul001@gmail.com>
2022-06-26 21:17:34 +0600

    seeder updated

--- backend/database/seeders/UserMessageSeeder.php
+++ backend/database/seeders/UserMessageSeeder.php
@@ -17,6 +17,12 @@ class UserMessageSeeder extends Seeder
     public function run()
     {
         // create 50 messages for each user
-        Message::factory(50)->create();
+        for ($i = 1; $i <= 50; $i++) {
+            Message::factory()->create([
+                'sender_id' => ($i%2) + 1,
+                'receiver_id' => (!($i%2)) + 1,
+            ]);
+        }
+
     }
 }
